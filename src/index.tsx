7883ef8aa6125ada3294db89f8f5f66a4f741fa2
dkmiecik <damian@productbrew.com>
2021-10-25 17:14:28 +0200

    Update React Native Reanimated v2.2.

--- src/index.tsx
+++ src/index.tsx
@@ -1,71 +1,15 @@
-import React from "react"
-import { StyleSheet, ViewStyle } from "react-native"
-import Animated, { Easing } from "react-native-reanimated"
-
-const {
-  Value,
-  Clock,
-  useCode,
-  cond,
-  set,
-  concat,
-  interpolate,
-  eq,
-  block,
-  not,
-  clockRunning,
-  startClock,
-  stopClock,
-  timing,
-} = Animated
-
-function runTiming(
-  clock: Animated.Clock,
-  value: Animated.Adaptable<number>,
-  dest: Animated.Adaptable<number>
-) {
-  const state = {
-    finished: new Value(0),
-    position: new Value(0),
-    time: new Value(0),
-    frameTime: new Value(0),
-  }
-
-  const config = {
-    duration: 500,
-    toValue: new Value(0),
-    easing: Easing.inOut(Easing.ease),
-  }
-
-  return block([
-    cond(
-      clockRunning(clock),
-      [
-        // if the clock is already running we update the toValue, in case a new dest has been passed in
-        set(config.toValue, dest),
-      ],
-      [
-        // if the clock isn't running we reset all the animation params and start the clock
-        set(state.finished, 0),
-        set(state.time, 0),
-        set(state.position, value),
-        set(state.frameTime, 0),
-        set(config.toValue, dest),
-        startClock(clock),
-      ]
-    ),
-    // we run the step here that is going to update position
-    timing(clock, state, config),
-    // if the animation is over we stop the clock
-    cond(state.finished, stopClock(clock)),
-    // we made the block return the updated position
-    state.position,
-  ])
-}
+import Animated, {
+  withTiming,
+  useAnimatedStyle,
+  useDerivedValue,
+  Easing,
+} from 'react-native-reanimated';
+import {ViewStyle, StyleSheet} from 'react-native';
+import React from 'react';
 
 type Props = {
   perspective?: number
-  side: 1 | 0
+  side: number
   rotate?: "Y" | "X"
   style?: ViewStyle
   front: React.ReactElement
@@ -73,100 +17,117 @@ type Props = {
 }
 
 const ReanimatedFlip = ({
-  perspective = 1200,
-  rotate = "Y",
-  side,
-  front,
-  back,
-  style,
-}: Props) => {
-  const { flipPosition, clock } = React.useMemo(
-    () => ({
-      flipPosition: new Value(side),
-      clock: new Clock(),
-    }),
-    []
-  )
+                          perspective = 1200,
+                          rotate = "Y",
+                          side,
+                          front,
+                          back,
+                          style,
+                        }: Props) => {
 
-  useCode(
-    () =>
-      block([
-        cond(not(eq(side, flipPosition)), [
-          set(
-            flipPosition,
-            runTiming(clock, flipPosition, side)
-            // spring({ clock: clock, from: flipPosition, to: side })
-          ),
-        ]),
-      ]),
-    [side]
-  )
+  const rotatePosition = Animated.interpolate(side, [0, 1],
+      [0, 180])
 
-  const rotatePosition = interpolate(flipPosition, {
-    inputRange: [0, 1],
-    outputRange: [0, 180],
-  })
-  const rotateValue = concat(rotatePosition, "deg")
+  const rotateX = useDerivedValue(() => {
+    return withTiming(rotatePosition, {
+      duration: 500,
+      easing: Easing.inOut(Easing.ease),
+    });
+  });
 
-  const opacityFront = interpolate(flipPosition, {
-    inputRange: [0.5, 0.51],
-    outputRange: [1, 0],
-    extrapolate: Animated.Extrapolate.CLAMP,
-  })
+  const rotationFlip = useDerivedValue(() => {
+    if (rotate === 'Y') {
+      return {
+        rotateY: `${rotateX.value}deg`
+      };
+    }
 
-  const opacityBack = interpolate(flipPosition, {
-    inputRange: [0.5, 0.51],
-    outputRange: [0, 1],
-    extrapolate: Animated.Extrapolate.CLAMP,
-  })
+    return {
+      rotateX: `${rotateX.value}deg`
+    };
+  }, [rotate, rotateX]);
 
-  const scaleFront = interpolate(flipPosition, {
-    inputRange: [0.5, 0.51],
-    outputRange: [1, 0],
-    extrapolate: Animated.Extrapolate.CLAMP,
-  })
+  const rotationFlipBack = useDerivedValue(() => {
+    if (rotate === 'Y') {
+      return {
+        rotateY: '180deg'
+      };
+    }
 
-  const scaleBack = interpolate(flipPosition, {
-    inputRange: [0.5, 0.51],
-    outputRange: [0, 1],
-    extrapolate: Animated.Extrapolate.CLAMP,
-  })
+    return {
+      rotateX: '180deg'
+    };
+  }, [rotate]);
+
+  const opacityFront = useDerivedValue(() => {
+    return Animated.interpolate(side,
+        [0.5, 0.51],
+        [1, 0],
+        Animated.Extrapolate.CLAMP);
+  }, [side]);
+
+  const opacityBack = useDerivedValue(() => {
+    return Animated.interpolate(side,
+        [0.5, 0.51],
+        [0, 1],
+        Animated.Extrapolate.CLAMP);
+  }, [side]);
+
+  const scaleFront = useDerivedValue(() => {
+    return Animated.interpolate(side,
+        [0.5, 0.51],
+        [1, 0],
+        Animated.Extrapolate.CLAMP);
+  }, [side]);
+
+  const scaleBack = useDerivedValue(() => {
+    return Animated.interpolate(side,
+        [0.5, 0.51],
+        [0, 1],
+        Animated.Extrapolate.CLAMP);
+  }, [side]);
+
+  const animatedStyleFront = useAnimatedStyle(() => {
+    return {
+      opacity: opacityFront.value,
+      transform: [
+        {perspective},
+        {...rotationFlip.value},
+        {
+          scale: scaleFront.value,
+        },
+      ],
+    };
+  }, [rotate, side, rotationFlip]);
+
+  const animatedStyleBack = useAnimatedStyle(() => {
+    return {
+      opacity: opacityBack.value,
+      transform: [
+        {perspective},
+        {...rotationFlipBack.value},
+        {...rotationFlip.value},
+        {
+          scale: scaleBack.value,
+        },
+      ],
+    };
+  }, [rotate, side]);
 
   return (
-    <Animated.View style={StyleSheet.flatten([style, styles.container])}>
-      <Animated.View
-        style={[
-          styles.side,
-          {
-            opacity: opacityFront,
-            transform: [
-              { perspective },
-              { [`rotate${rotate}`]: rotateValue },
-              { scale: scaleFront },
-            ],
-          },
-        ]}
-      >
-        {front}
+      <Animated.View style={StyleSheet.flatten([style, styles.container])}>
+        <Animated.View
+            style={[styles.side, animatedStyleFront]}
+        >
+          {front}
+        </Animated.View>
+        <Animated.View
+            style={[styles.side, animatedStyleBack]}
+        >
+          {back}
+        </Animated.View>
       </Animated.View>
-      <Animated.View
-        style={[
-          styles.side,
-          {
-            opacity: opacityBack,
-            transform: [
-              { perspective },
-              { [`rotate${rotate}`]: "180deg" },
-              { [`rotate${rotate}`]: rotateValue },
-              { scale: scaleBack },
-            ],
-          },
-        ]}
-      >
-        {back}
-      </Animated.View>
-    </Animated.View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
@@ -178,7 +139,9 @@ const styles = StyleSheet.create({
     width: "100%",
     height: "100%",
     position: "absolute",
+    justifyContent: "center",
+    alignItems: "center",
   },
-})
+});
 
-export default ReanimatedFlip
+export default ReanimatedFlip;
